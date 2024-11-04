ebfd5a8b6dcfdbbf240a641fd5629b0ad9c14124
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2024-11-03 21:04:36 -0300

    fix: cors activate cors to release requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // src/app.js
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
 import cardRoutes from './routes/cardRoutes.js';
@@ -7,6 +8,7 @@ import cardRoutes from './routes/cardRoutes.js';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
